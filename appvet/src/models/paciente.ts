ccc17e7a1bfef2099f96fe1688812e41cd48350d
celada182 <celada.jcm.javier@gmail.com>
2017-05-20 19:11:35 +0200

    Detalles de Paciente
    Agregada dirección

--- appvet/src/models/paciente.ts
+++ appvet/src/models/paciente.ts
@@ -2,6 +2,7 @@ import {Consulta} from "./consulta";
 export class Paciente {
   nombre: string;
   propietario: string;
+  direccion:string;
   microchip: number;
   especie: string;
   fechaNacimiento: string;
--- appvet/src/pages/paciente-consultas/paciente-consultas.ts
+++ appvet/src/pages/paciente-consultas/paciente-consultas.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import {App, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {AlertController, App, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {PacientesPage} from "../pacientes/pacientes";
 import {Paciente} from "../../models/paciente";
 import {NuevaConsultaPage} from "../nueva-consulta/nueva-consulta";
+import {PacientesService} from "../../services/pacientes.service";
 
 @IonicPage()
 @Component({
@@ -12,21 +13,39 @@ import {NuevaConsultaPage} from "../nueva-consulta/nueva-consulta";
 export class PacienteConsultasPage {
 
   nuevaConsultaPage = NuevaConsultaPage;
-  paciente:Paciente;
+  paciente: Paciente;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private app:App) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private app: App,
+              private pacientesService: PacientesService,
+              private alertCtrl: AlertController) {
     this.paciente = this.navParams.data;
   }
 
-  onBackToPacientes(){
-    this.app.getRootNav().setRoot(PacientesPage,{},{
-      animate:true,
-      direction:'back'
+  onBackToPacientes() {
+    this.app.getRootNav().setRoot(PacientesPage, {}, {
+      animate: true,
+      direction: 'back'
     });
   }
 
-  onDelete(){
-
+  onDelete(index) {
+    let confirm = this.alertCtrl.create({
+      title: '¿Estas seguro de eliminar la consulta?',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            this.pacientesService.deleteConsulta(this.paciente, index);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
